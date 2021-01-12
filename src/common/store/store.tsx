06a1fa6eb74c34ccbf26cf282941a2d2bd860ca7
wenjiechen <wendychen9395@gmail.com>
2021-01-12 12:48:39 -0500

    project boilerplate: add ts + eslint + prettier + materialui + redux + router

---
+++ src/common/store/store.tsx
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware, compose, Action } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk, { ThunkAction } from 'redux-thunk';
+// import moviesReducer from '../movies/moviesReducer';
+// import customBurgerReducer from './customBurgerStore/customBurgerReducer';
+
+// const composeEnhancers = composeWithDevTools({}) || compose;
+
+// const rootReducer = combineReducers({
+//   movies: moviesReducer,
+// });
+
+// export type RootState = ReturnType<typeof rootReducer>;
+
+// export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+
+// export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
